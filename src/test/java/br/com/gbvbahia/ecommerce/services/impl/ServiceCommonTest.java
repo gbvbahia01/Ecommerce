1ad1ec02681d51782e0b3e8bfd7b1a07861b3fa9
Guilherme Bahia <gbvbahia01@gmail.com>
2018-04-08 07:20:14 -0300

    Service Common Test

---
+++ src/test/java/br/com/gbvbahia/ecommerce/services/impl/ServiceCommonTest.java
@@ -0,0 +1,49 @@
+package br.com.gbvbahia.ecommerce.services.impl;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ServiceCommonTest {
+
+
+    private ServiceCommon serviceCommon;
+
+    @Before
+    public void setUp() {
+        serviceCommon = new ServiceCommon() { };
+    }
+
+    @Test
+    public void stringCleanForLikeINITTest() {
+        String parameterSearch = "NõtWêllLîkéPÃramèter";
+        String expectedParameter = "%notwelllikeparameter";
+
+        StringBuilder result = serviceCommon.stringCleanForLike(ServiceCommon.Like.INIT, parameterSearch);
+
+        Assert.assertEquals("Like.INIT fails", result.toString(), expectedParameter);
+
+    }
+
+    @Test
+    public void stringCleanForLikeBETWEENTest() {
+        String parameterSearch = "NõtWêllLîkéPÃramèter";
+        String expectedParameter = "%notwelllikeparameter%";
+
+        StringBuilder result = serviceCommon.stringCleanForLike(ServiceCommon.Like.BETWEEN, parameterSearch);
+
+        Assert.assertEquals("Like.BETWEEN fails", result.toString(), expectedParameter);
+
+    }
+
+    @Test
+    public void stringCleanForLikeENDTest() {
+        String parameterSearch = "NõtWêllLîkéPÃramèter";
+        String expectedParameter = "notwelllikeparameter%";
+
+        StringBuilder result = serviceCommon.stringCleanForLike(ServiceCommon.Like.END, parameterSearch);
+
+        Assert.assertEquals("Like.END fails", result.toString(), expectedParameter);
+
+    }
+}
