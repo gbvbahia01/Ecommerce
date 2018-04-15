6afd08933b626dcdb3c5e0c40ad966f9cde6babe
Guilherme Bahia <gbvbahia01@gmail.com>
2018-04-15 11:53:41 -0300

    New packages path

---
+++ src/main/java/br/com/gbvbahia/ecommerce/services/ServiceCommon.java
@@ -0,0 +1,52 @@
+package br.com.gbvbahia.ecommerce.services;
+
+import br.com.gbvbahia.ecommerce.util.StringHelper;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.repository.CrudRepository;
+
+public abstract class ServiceCommon<T, ID, R extends CrudRepository<T, ID>> implements ServiceContract<T, ID> {
+
+    //=========
+    // Contract
+    //=========
+    protected abstract R getRepository();
+    
+    //========
+    // Helpers
+    //========
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+    
+    protected enum Like {
+        INIT, END, BETWEEN;
+    }
+     
+    /**
+     * Put % at init and at the end of String.
+     * @param parameter to put between %%.
+     * @return %parameter%.
+     */
+    protected StringBuilder stringCleanForLike(Like likeDef, String parameter) {
+
+        StringBuilder builder = new StringBuilder();
+
+        if (Like.INIT.equals(likeDef) || Like.BETWEEN.equals(likeDef)) {
+            builder.append("%");
+        }
+
+        builder.append(StringHelper.cleanToSearch(parameter));
+
+        if (Like.END.equals(likeDef) || Like.BETWEEN.equals(likeDef)) {
+            builder.append("%");
+        }
+        return builder;
+    }
+    
+    //===================
+    // Repository Methods
+    //===================
+    public Optional<T> findBydId(ID id) {
+        return getRepository().findById(id);
+    }
+}
---
+++ src/main/java/br/com/gbvbahia/ecommerce/services/products/ProductService.java
@@ -0,0 +1,24 @@
+package br.com.gbvbahia.ecommerce.services.products;
+
+import br.com.gbvbahia.ecommerce.model.entity.products.Product;
+import br.com.gbvbahia.ecommerce.services.ServiceContract;
+
+import java.util.List;
+
+/**
+ * Project: Ecommerce
+ *
+ * @author Guilherme
+ * @version 1.0
+ * @since 15/04/18
+ */
+public interface ProductService extends ServiceContract<Product, Long> {
+
+    /**
+     *
+     * Search products using nameClean and descriptionClean.
+     * @param search a string that will be used to search at both attributes.
+     * @return A list with products. Empty if not found.
+     */
+    List<Product> search(String search);
+}
